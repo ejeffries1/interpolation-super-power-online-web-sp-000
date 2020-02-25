a12c570c435c6cd8b0dc239181bcebc591ff4f23
Evan Jeffries <ejeffries14@gmail.com>
2020-02-25 17:34:42 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,12 +2,5 @@
 require "pry"
 
 def display_rainbow(colors)
-
-  print "#{colors[0][0]}".upcase + ": #{colors[0]}, "
-  print "#{colors[1][0]}".upcase + ": #{colors[1]}, "
-  print "#{colors[2][0]}".upcase + ": #{colors[2]}, "
-  print "#{colors[3][0]}".upcase + ": #{colors[3]}, "
-  print "#{colors[4][0]}".upcase + ": #{colors[4]}, "
-  print "#{colors[5][0]}".upcase + ": #{colors[5]}, "
-  print "#{colors[6][0]}".upcase + ": #{colors[6]}"
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
